69e765317d78aa12889b3651261763472430325e
NS0H <bory1610@gmail.com>
2024-06-12 03:04:54 +0900

    feat: atom 생성

--- src/atom/stats.ts
+++ src/atom/stats.ts
@@ -1,1 +1,18 @@
+import { User } from "@/types/User";
+import { Study } from "@/types/aboutStudy";
 import { atom } from "recoil";
+
+export const userState = atom<User | null>({
+  key: "userState",
+  default: null,
+});
+
+export const studiesState = atom<Study[]>({
+  key: "studiesState",
+  default: [],
+});
+
+export const selectedStudyState = atom<Study | null>({
+  key: "selectedStudyState",
+  default: null,
+});
