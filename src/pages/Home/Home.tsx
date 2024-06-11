89489532ac4878a977818512aeed89f37823cc68
NS0H <bory1610@gmail.com>
2024-06-11 12:19:20 +0900

    refactor: 코드 정리

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,21 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import axios from "axios";
+
 //styles
 import { Container } from "@/styles/home/homeStyles";
 
 //img
-import GoormThinking from "@/assets/home/goormThinking.jpg";
-import Plus from "@/assets/home/plus.png";
 import Expansion from "@/assets/home/expansion.png";
-
-// theme
-
-// libraries
 
 // components
 import CodeReview from "@/components/CodeReview";
-import StudyList from "@/components/Study/StudyList";
+import StudyList from "@/components/home/StudyList";
 
 // types
 import { User } from "@/types/User";
 import { Study } from "@/types/aboutStudy";
-import StudyInformation from "@/components/StudyInformation";
 import AlgorithmList from "@/components/AlgorithmList";
 import HamburgerBar from "@/components/home/HamburgerBar";
 
@@ -39,10 +34,13 @@ import useGetCategoryData from "@/hooks/home/useGetCategoryData";
 
 const Home = () => {
   const navigate = useNavigate();
+
+  const [user, setUser] = useState<User | null>(null);
+  const [studies, setStudies] = useState<Study[]>([]);
+  const [selectedStudy, setSelectedStudy] = useState<Study | null>(null);
+
   // 햄버거바 컨트롤
   const [showHamburgerBar, setShowHamburgerBar] = useState(false);
-
-  const [selectedStudy, setSelectedStudy] = useState<Study | null>(null);
 
   // 카테고리 데이터
   const [categoryList, setCategoryList] = useState<CategoryListMap>({});
@@ -93,31 +91,17 @@ const Home = () => {
       console.log("study List : ", data);
       setStudies(data);
     } catch (e) {
-      console.log(e);
+      console.log("study List error : ", e);
     }
   }
 
-  const getUserData = async () => {
-    try {
-      const response = await axios.get("/user");
-      const data = response.data;
-      console.log(data);
-      setUser(data);
-    } catch (e) {
-      console.log(e);
+  const addStudy = (newStudy: Study) => {
+    if (studies) {
+      setStudies([...studies, newStudy]);
+    } else {
+      setStudies([newStudy]);
     }
-  }
-
-  const getStudyList = async () => {
-    try {
-      const response = await axios.get("/studyList");
-      const data = response.data;
-      console.log("study List : ", data);
-      setStudies(data);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+  };
 
   const getCategoryData = async () => {
     const object = {
@@ -139,23 +123,11 @@ const Home = () => {
     execute();
   };
 
-  const addStudy = (newStudy: Study) => {
-    if (studies) {
-      setStudies([...studies, newStudy]);
-    } else {
-      setStudies([newStudy]);
-    }
-  
-    // 서버나 다른 데이터 저장소에 새 스터디를 저장하는 로직을 추가할 수 있습니다.
-    // 예를 들어:
-    // await axios.post('/studies', newStudy);
-  };
-
-
   const handleStudySelect = (study: Study) => {
     setSelectedStudy(study);
     setShowHamburgerBar(true);
   };
+
   // 햄버거바 컨트롤 함수
   const handleHamburgerBar = () => setShowHamburgerBar(!showHamburgerBar);
 
@@ -175,9 +147,9 @@ const Home = () => {
   }, [pageData, problemList]);
 
   useEffect(() => {
-    getCategoryData();
     getUserData();
     getStudyList();
+    getCategoryData();
   }, []);
 
   return (
@@ -186,81 +158,17 @@ const Home = () => {
         <div className="header">구름적사고</div>
       </nav>
       <main>
-      <StudyList 
-        studies={studies || []} 
-        addStudy={addStudy} 
-        user={user}
-        onStudySelect={handleStudySelect}
-      />
-        <div className="hamburgerBarContainer">
-        <StudyInformation 
-          study={selectedStudy}
-          showHamburgerBar={showHamburgerBar}
-          handleHamburgerBar={handleHamburgerBar}
-          theme={theme}
+        <StudyList 
+          studies={studies || []} 
+          addStudy={addStudy} 
           user={user}
+          onStudySelect={handleStudySelect}
         />
-          <div className="drawerContent">
-            <div className="categorySpace">
-              <div className="algorithmList">알고리즘 목록</div>
-              {Object.entries(categoryList).map(
-                ([listName, value], idx: number) => {
-                  return (
-                    <div className="categoryRow" key={listName}>
-                      <div style={{ display: "flex", alignItems: "center" }}>
-                        <span style={{ marginRight: "0.5rem" }}>
-                          {listName}
-                        </span>
-                        <span>-----------</span>
-                        <img
-                          style={{ marginLeft: "0.5rem" }}
-                          title="week1"
-                          src={
-                            isToggleSelected[idx]
-                              ? CategoryExpansion
-                              : CategoryExpansion2
-                          }
-                          onClick={() => handleToggle(idx)}
-                        />
-                      </div>
-
-                      {isToggleSelected[idx] &&
-                        value.map((item: CategoryListData, index: number) => (
-                          <div key={index} className="algorithmProblems">
-                            <li
-                              style={{ padding: "0.3rem" }}
-                              onClick={() => handlePage(item)}
-                            >
-                              {item.subjectNumber} {item.subjectName}
-                            </li>
-                          </div>
-                        ))}
-                    </div>
-                  );
-                }
-              )}
-            </div>
-          </div>
-          <div className="drawerButton">
-            <img
-              src={LogOut}
-              style={{ width: "20px", height: "20px", marginBottom: "0.5rem" }}
-            />
-          </div>
-        </div>
-        {/* 소희님 작업 부분 */}
-        <div className="drawer">
-          <img src={GoormThinking} className="element1" />
-          <img src={GoormThinking} className="element1" />
-          <img src={GoormThinking} className="element1" />
-          <div className="plusContainer">
-            <img src={Plus} />
-          </div>
-          {/* 소희님 작업 부분 */}
-        </div>
         <HamburgerBar
           handleHamburgerBar={handleHamburgerBar}
           showHamburgerBar={showHamburgerBar}
+          user={user}
+          study={selectedStudy}
           categoryList={categoryList}
           isToggleSelected={isToggleSelected}
           handleToggle={handleToggle}
