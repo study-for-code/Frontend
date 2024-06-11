4e4aced6e4dd46280b85f0770c5d15af958a3f6b
NS0H <bory1610@gmail.com>
2024-06-12 03:07:11 +0900

    feat: 스터디 삭제 및 입장 구현, recoil 사용

--- src/components/home/HamburgerBar.tsx
+++ src/components/home/HamburgerBar.tsx
@@ -1,3 +1,5 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { useEffect, useState } from "react";
 // theme
 import { theme } from "@/styles/common/ColorStyles";
 
@@ -6,19 +8,20 @@ import LogOut from "@/assets/home/logout.png";
 import Hamburger from "@/assets/home/hamburgerBar.png";
 import CategoryExpansion from "@/assets/home/category_expansion.png";
 import CategoryExpansion2 from "@/assets/home/category_expansion2.png";
+import Pen from "@/assets/home/pen.png";
+import Trash from "@/assets/home/trash.png";
 
 // types
 import { CategoryListData, CategoryListMap } from "@/types/aboutHome";
-import { Study } from "@/types/aboutStudy";
 import { User } from "@/types/User";
+import { selectedStudyState, studiesState, userState } from "@/atom/stats";
+import DeleteStudyModal from "../modal/DeleteStudyModal";
 
 interface HamburgerBarType {
   handleHamburgerBar: () => void;
   handleToggle: (idx: number) => void;
   handlePage: (data: CategoryListData) => void;
   showHamburgerBar: boolean;
-  study: Study | null;
-  user: User | null;
   categoryList: CategoryListMap;
   isToggleSelected: boolean[];
   goToLoginPage: () => void;
@@ -27,82 +30,168 @@ interface HamburgerBarType {
 const HamburgerBar: React.FC<HamburgerBarType> = ({
   handleHamburgerBar,
   showHamburgerBar,
-  user,
-  study,
   categoryList,
   isToggleSelected,
   handleToggle,
   handlePage,
   goToLoginPage,
 }) => {
+  const user = useRecoilValue(userState);
+  const [studies, setStudies] = useRecoilState(studiesState); // Recoil 상태에서 studies 가져오기
+  const [selectedStudy, setSelectedStudy] = useRecoilState(selectedStudyState);
+  const [isEditing, setIsEditing] = useState(false); // 제목 수정 모드 상태
+  const [newTitle, setNewTitle] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태 추가
 
-  let formattedDate = 'Invalid Date';
-  if (study) {
+  let formattedDate = "Invalid Date";
+  if (selectedStudy) {
     try {
-      let createAtDate = new Date(study.createAt); // Date 객체로 변환
-      if (!isNaN(createAtDate.getTime())) { // 유효한 날짜인지 확인
-        formattedDate = createAtDate.toLocaleDateString('ko-KR', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit'
+      let createAtDate = new Date(selectedStudy.createAt); // Date 객체로 변환
+      if (!isNaN(createAtDate.getTime())) {
+        formattedDate = createAtDate.toLocaleDateString("ko-KR", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
         });
       }
     } catch (error) {
-      console.error('Invalid date format:', error);
+      console.error("Invalid date format:", error);
     }
   }
+
+  useEffect(() => {
+    if (selectedStudy) {
+      setNewTitle(selectedStudy.title);
+    }
+  }, [selectedStudy]);
+
+  const handleTitleEdit = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsEditing(true);
+  };
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTitle(event.target.value);
+  };
+
+  const handleTitleSave = (event: React.MouseEvent) => {
+    event.stopPropagation(); // 이벤트 버블링 방지
+    if (selectedStudy) {
+      const updatedStudies = studies.map((s) =>
+        // 추후 스터디 ID로 수정 필요
+        s.host === selectedStudy.host ? { ...s, title: newTitle } : s
+      );
+      setStudies(updatedStudies);
+      setSelectedStudy({ ...selectedStudy, title: newTitle });
+      setIsEditing(false);
+    }
+  };
+
+  const handleDeleteStudy = () => {
+    if (selectedStudy) {
+      const updatedStudies = studies.filter(
+        // 추후 스터디 ID로 수정
+        (s) => s.title !== selectedStudy.title
+      );
+      setStudies(updatedStudies);
+      setSelectedStudy(null); // 선택된 스터디 초기화
+      handleHamburgerBar();
+    }
+  };
+
+  const handleDeleteClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsModalOpen(true); // 모달 열기
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false); // 모달 닫기
+  };
+
+  const handleModalConfirm = () => {
+    handleDeleteStudy();
+    setIsModalOpen(false); // 모달 닫기
+  };
 
   return (
     <div className="hamburgerBarContainer">
       <div className="drawerSection" onClick={handleHamburgerBar}>
         <div className="hamburgerbutton">
-          {showHamburgerBar && study && (
+          {showHamburgerBar && selectedStudy && (
             <div className="studyName">
-              {study.image && (
-              <img
-                src={URL.createObjectURL(study.image)}
-                style={{
-                  width: "25px",
-                  height: "25px",
-                  borderRadius: "50%",
-                  marginRight: "0.5rem",
-                  marginLeft: "0.5rem",
-                }}
-                alt="study"
-              />
-            )}
-              <span
-                style={{
-                  fontSize: "1.2rem",
-                  color: `${theme.fontWhiteColor}`,
-                }}
-              >
-                {study.title}
-              </span>
-              {user && study.host && (user.email === study.host.email) && (
-              <button style={{
-                marginLeft: "0.5rem",
-                fontSize: "1rem",
-                background: "none",
-                border: "none",
-                color: `${theme.fontWhiteColor}`,
-                cursor: "pointer"
-              }}>
-                수정
-              </button>
-            )}
+              {selectedStudy.image && (
+                <img
+                  src={URL.createObjectURL(selectedStudy.image)}
+                  style={{
+                    width: "25px",
+                    height: "25px",
+                    borderRadius: "50%",
+                    marginRight: "0.5rem",
+                    marginLeft: "0.5rem",
+                  }}
+                  alt="study"
+                />
+              )}
+              {isEditing ? (
+                <>
+                  <input
+                    type="text"
+                    value={newTitle}
+                    onChange={handleTitleChange}
+                    onClick={(e) => e.stopPropagation()}
+                    style={{ width: "50px" }}
+                  />
+                  <button onClick={handleTitleSave}>Save</button>
+                </>
+              ) : (
+                <>
+                  <span
+                    style={{
+                      fontSize: "1.2rem",
+                      color: `${theme.fontWhiteColor}`,
+                    }}
+                  >
+                    {selectedStudy.title}
+                  </span>
+                  {user &&
+                    selectedStudy.host &&
+                    user.email === selectedStudy.host.email && (
+                      <>
+                        <img
+                          src={Pen}
+                          className="updateStudyName"
+                          onClick={handleTitleEdit}
+                          alt="Edit"
+                        />
+                        <img
+                          src={Trash}
+                          className="updateStudyName"
+                          onClick={handleDeleteClick}
+                          alt="Delete"
+                        />
+                      </>
+                    )}
+                </>
+              )}
             </div>
           )}
 
           <img src={Hamburger} style={{ marginRight: "1rem" }} />
         </div>
-        {showHamburgerBar && study && (
+        {showHamburgerBar && selectedStudy && (
           <div className="StudyContent">
             <div>개설 날짜: {formattedDate}</div>
-            <div>개설자: {study.host?.nickname}</div>
+            <div>개설자: {selectedStudy.host?.nickname}</div>
           </div>
         )}
       </div>
+      {/* {isDeleting && (
+        <div className="deleteConfirmation">
+          <p>{`"${selectedStudy?.title}" 스터디를 정말 삭제하시겠습니까?`}</p>
+          <button onClick={handleDeleteStudy}>예</button>
+          <button onClick={handleCancelDelete}>아니오</button>
+        </div>
+      )} */}
       <div className="drawerContent">
         <div className="categorySpace">
           <div className="algorithmList">알고리즘 목록</div>
@@ -149,6 +238,13 @@ const HamburgerBar: React.FC<HamburgerBarType> = ({
           onClick={goToLoginPage}
         />
       </div>
+
+      <DeleteStudyModal
+        isOpen={isModalOpen}
+        onClose={handleModalClose}
+        onConfirm={handleModalConfirm}
+        studyTitle={selectedStudy?.title || ""}
+      />
     </div>
   );
 };
--- src/components/home/StudyList.tsx
+++ src/components/home/StudyList.tsx
@@ -1,87 +1,120 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 import GoormThinking from "@/assets/home/goormThinking.jpg";
 import Plus from "@/assets/home/plus.png";
-import CreateStudyModal from '../modal/CreateStudyModal';
-import { Study } from '@/types/aboutStudy';
-import { User } from '@/types/User';
+import CreateStudyModal from "../modal/CreateStudyModal";
+import { Study } from "@/types/aboutStudy";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { selectedStudyState, studiesState, userState } from "@/atom/stats";
+import EnterStudyModal from "../modal/EnterStudyModal";
 
-interface StudyListProps {
-  studies: Study[];
-  addStudy: (newStudy: Study) => void;
-  user: User | null;
-  onStudySelect: (study: Study) => void;
-}
-
-const StudyList = ({ studies, addStudy, user, onStudySelect }: StudyListProps) => {
+const StudyList = () => {
+  const user = useRecoilValue(userState);
+  const studies = useRecoilValue(studiesState);
+  const setStudies = useSetRecoilState(studiesState);
+  const setSelectedStudy = useSetRecoilState(selectedStudyState);
   const [showOptions, setShowOptions] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+  const [isEnterModalOpen, setIsEnterModalOpen] = useState(false);
   const containerRef = useRef<HTMLDivElement>(null);
 
   const handlePlusClick = () => {
-      setShowOptions(prevState => !prevState);
+    setShowOptions((prevState) => !prevState);
   };
 
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
+  const handleOpenCreateModal = () => {
+    setIsCreateModalOpen(true);
   };
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
+  const handleCloseCreateModal = () => {
+    setIsCreateModalOpen(false);
   };
 
-  const handleAddStudy = (study: Study) => {
-    addStudy(study);
+  const handleAddStudy = (newStudy: Study) => {
+    setStudies((prevStudies) => [...prevStudies, newStudy]);
+    setSelectedStudy(newStudy);
+  };
+
+  const handleSelectStudy = (study: Study) => {
+    setSelectedStudy(study);
+  };
+
+  const handleOpenEnterModal = () => {
+    setIsEnterModalOpen(true);
+  };
+
+  const handleCloseEnterModal = () => {
+    setIsEnterModalOpen(false);
+  };
+
+  const handleEnterStudy = (study: Study) => {
+    // 추후 스터디 ID로 수정
+    const isStudyInList = studies.some((s) => s.title === study.title);
+    if (!isStudyInList) {
+      setStudies((prevStudies) => [...prevStudies, study]);
+      setSelectedStudy(study);
+    }
   };
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target as Node)
+      ) {
         setShowOptions(false);
       }
     };
 
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [containerRef]);
 
-    return (
-        <div className="drawer" ref={containerRef}>
-          <div>
-            {studies.map((study, index) => (
-              <div key={index} onClick={() => onStudySelect(study)}>
-                <img 
-                  src={study.image ? URL.createObjectURL(study.image) : GoormThinking} 
-                  alt={study.title} 
-                  width="100" 
-                  className="element1"
-                />
-              </div>
-            ))}
+  return (
+    <div className="drawer" ref={containerRef}>
+      <div>
+        {studies.map((study, index) => (
+          <div key={index} onClick={() => handleSelectStudy(study)}>
+            <img
+              src={
+                study.image ? URL.createObjectURL(study.image) : GoormThinking
+              }
+              alt={study.title}
+              width="100"
+              className="element1"
+            />
           </div>
-          <div className="plusContainer">
-            <img src={Plus} onClick={handlePlusClick} />
-            {showOptions && (
-            <div className="optionsContainer">
-              <button className="optionButton" onClick={handleOpenModal}>
-                스터디 생성
-              </button>
-              <button className="optionButton" onClick={() => alert('스터디 입장')}>
-                스터디 입장
-              </button>
-            </div>
-          )}
+        ))}
+      </div>
+      <div className="plusContainer">
+        <img src={Plus} onClick={handlePlusClick} />
+        {showOptions && (
+          <div className="optionsContainer">
+            <button className="optionButton" onClick={handleOpenCreateModal}>
+              스터디 생성
+            </button>
+            <button className="optionButton" onClick={handleOpenEnterModal}>
+              스터디 입장
+            </button>
           </div>
-          
-          <CreateStudyModal
-            isOpen={isModalOpen} 
-            onClose={handleCloseModal}
-            onSubmit={handleAddStudy} 
-            user={user}
-          />
-        </div>
-    );
-  };
-  
-  export default StudyList;
+        )}
+      </div>
+
+      <CreateStudyModal
+        isOpen={isCreateModalOpen}
+        onClose={handleCloseCreateModal}
+        onSubmit={handleAddStudy}
+        user={user}
+      />
+
+      <EnterStudyModal
+        isOpen={isEnterModalOpen}
+        onClose={handleCloseEnterModal}
+        onEnterStudy={handleEnterStudy}
+      />
+    </div>
+  );
+};
+
+export default StudyList;
