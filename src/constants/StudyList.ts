7ada9f6132d8f0fa0fb53be1c279d52cf3ab6e27
NS0H <bory1610@gmail.com>
2024-06-12 03:08:49 +0900

    test: 테스트 데이터 추가

--- src/constants/StudyList.ts
+++ src/constants/StudyList.ts
@@ -1,11 +1,35 @@
 export const studyList = [
-    {
-        title: 'study 1',
-        createAt: new Date('2024-06-01'),
-        image: null,
-        host: {
-            email: 'bory1610@gmail.com',
-            nickname: 'NSH'
-        }
-    }
-]
+  {
+    title: "study 1",
+    createAt: new Date("2024-06-01"),
+    image: null,
+    host: {
+      email: "bory1610@gmail.com",
+      nickname: "NSH",
+    },
+    code: "123",
+  },
+  {
+    title: "study 2",
+    createAt: new Date("2024-06-05"),
+    image: null,
+    host: {
+      email: "bory1610@naver.com",
+      nickname: "sohee",
+    },
+    code: "456",
+  },
+];
+
+export const fullStudyList = [
+  {
+    title: "new Study",
+    createAt: new Date("2024-05-05"),
+    image: null,
+    host: {
+      email: "bory1610@naver.com",
+      nickname: "sohee",
+    },
+    code: "789",
+  },
+];
