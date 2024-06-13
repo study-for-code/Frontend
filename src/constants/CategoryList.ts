e9dbc50184dfb4e8295ceffe32b83240ee9dad0a
NS0H <bory1610@gmail.com>
2024-06-13 16:59:12 +0900

    feat: Category 임의 데이터 API

---
+++ src/constants/CategoryList.ts
@@ -0,0 +1,34 @@
+export const categoryList = [
+  // study 1의 카테고리
+  {
+    category_id: 1,
+    title: "1주차 문제",
+    study_id: 1,
+  },
+  {
+    category_id: 2,
+    title: "2주차 문제",
+    study_id: 1,
+  },
+  {
+    category_id: 3,
+    title: "3주차 문제",
+    study_id: 1,
+  },
+  // study 2의 카테고리
+  {
+    category_id: 4,
+    title: "1주차 문제",
+    study_id: 2,
+  },
+  {
+    category_id: 5,
+    title: "2주차 문제",
+    study_id: 2,
+  },
+  {
+    category_id: 6,
+    title: "3주차 문제",
+    study_id: 2,
+  },
+];
