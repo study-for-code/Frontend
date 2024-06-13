0bac2441e1ba4a34ebe7e551b7ea6d17f34980c7
NS0H <bory1610@gmail.com>
2024-06-13 16:58:18 +0900

    feat: Add Category Interface

--- src/types/aboutStudy.ts
+++ src/types/aboutStudy.ts
@@ -1,10 +1,16 @@
 import { User } from "./User";
 
 export interface Study {
-  study_id: Number;
+  study_id: number;
   title: string;
   createAt: Date;
   image: File | null;
   host: User | null;
   code: string;
+}
+
+export interface Category {
+  category_id: number;
+  title: string;
+  study_id: number;
 }
