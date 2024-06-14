404170efc6a639c2a4f397d2c6dd6ab74e31d6a5
NS0H <bory1610@gmail.com>
2024-06-14 19:05:05 +0900

    feat: 카테고리 제목 수정 기능 구현

--- src/components/home/CategorySpace.tsx
+++ src/components/home/CategorySpace.tsx
@@ -4,7 +4,6 @@ import { createPortal } from "react-dom";
 // image
 import CategoryExpansion from "@/assets/home/category_expansion.png";
 import CategoryExpansion2 from "@/assets/home/category_expansion2.png";
-import Plus from "@/assets/home/plus_gray.png";
 
 // type
 import { TaskListData } from "@/types/aboutHome";
@@ -14,7 +13,7 @@ import { Category } from "@/types/aboutStudy";
 import CreateCategoryModal from "../modal/CreateCategoryModal";
 
 // atom
-import { useRecoilValue, useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilValue, useRecoilState } from "recoil";
 import {
   cgListState,
   fullCategoryListState,
@@ -43,26 +42,56 @@ const CategorySpace: React.FC<CategorySpaceProps> = ({
   const [fullCatagoryList, setFullCategoryList] = useRecoilState(
     fullCategoryListState
   );
-  const setCgList = useSetRecoilState(cgListState);
+  const [cgList, setCgList] = useRecoilState(cgListState);
   const selectedStudy = useRecoilValue(selectedStudyState);
   const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
-  const [showOptions, setShowOptions] = useState(false);
+  const [showOuterOptions, setShowOuterOptions] = useState(false);
+  const [showInnerOptions, setShowInnerOptions] = useState(false);
   const [menuPosition, setMenuPosition] = useState({ x: 0, y: 0 });
   const containerRef = useRef<HTMLDivElement>(null);
+  const [isEditTitle, setIsEditTitle] = useState(
+    new Array(categoryList.length).fill(false)
+  );
+  const [newTitle, setNewTitle] = useState<{ [key: number]: string }>({});
+  const [selectedCgID, setSelectedCgID] = useState(0);
 
   console.log("task list: ", taskList);
   console.log("category ID : ", categoryList);
   console.log("isToggleSelected: ", isToggleSelected);
 
+  const getHyphens = (length: number) => {
+    let num = 16 - length;
+    return "-".repeat(num);
+  };
+
   const handleCreateModal = () => {
-    setShowOptions(false);
+    setShowOuterOptions(false);
     setIsCreateModalOpen(!isCreateModalOpen);
   };
 
-  const handleContextMenu = (event: React.MouseEvent<HTMLDivElement>) => {
+  const handleOuterContextMenu = (event: React.MouseEvent<HTMLDivElement>) => {
     event.preventDefault();
     setMenuPosition({ x: event.clientX, y: event.clientY });
-    setShowOptions(true);
+    setShowOuterOptions(true);
+  };
+
+  const handleInnerContextMenu = (
+    event: React.MouseEvent<HTMLDivElement>,
+    category_id: number
+  ) => {
+    event.preventDefault();
+    setMenuPosition({ x: event.clientX, y: event.clientY });
+    setShowInnerOptions(true);
+    setSelectedCgID(category_id);
+  };
+
+  const handleCgTitleEdit = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    setIsEditTitle((prev) => {
+      const newEditTitle = [...prev];
+      newEditTitle[selectedCgID] = true;
+      return newEditTitle;
+    });
   };
 
   const handleAddCategory = (newCategory: Category) => {
@@ -70,27 +99,46 @@ const CategorySpace: React.FC<CategorySpaceProps> = ({
     setCgList((prevCg) => [...prevCg, newCategory]);
   };
 
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    category: Category
+  ) => {
+    setNewTitle((prev) => ({
+      ...prev,
+      [category.category_id]: event.target.value,
+    }));
+  };
+
+  // 카테고리 이름 수정 시 반영이 되기는 하나, 다른 스터디 선택 후 돌아오면 다시 초기화 됨
+  // -> 스터디 선택 시 카테고리 리스트를 다시 불러오기 때문
+  // 실제 API 연결 시 데이터 저장 필...
+  const handleEditTitle = (category_id: number) => {
+    const updatedFullCategories = fullCatagoryList.map((cg) =>
+      cg.category_id === category_id
+        ? { ...cg, title: newTitle[category_id] }
+        : cg
+    );
+
+    setFullCategoryList(updatedFullCategories);
+    const updatedCategories = cgList.map((cg) =>
+      cg.category_id === category_id
+        ? { ...cg, title: newTitle[category_id] }
+        : cg
+    );
+
+    setCgList(updatedCategories);
+    setIsEditTitle((prev) => {
+      const newEditTitle = [...prev];
+      newEditTitle[category_id] = false;
+      return newEditTitle;
+    });
+    setNewTitle((prev) => ({ ...prev, [category_id]: "" }));
+  };
+
   console.log("full Category : ", fullCatagoryList);
 
   const modalRoot = document.querySelector("#modal-container");
   if (!modalRoot) return null;
-
-  // useEffect(() => {
-  //   const container = containerRef.current;
-  //   if (showOptions && container) {
-  //     document.body.appendChild(container);
-  //     container.style.position = "absolute";
-  //     container.style.left = `${menuPosition.x}px`;
-  //     container.style.top = `${menuPosition.y}px`;
-  //     container.style.zIndex = "1000";
-  //   }
-
-  //   return () => {
-  //     if (container && document.body.contains(container)) {
-  //       document.body.removeChild(container);
-  //     }
-  //   };
-  // }, [showOptions, menuPosition]);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -98,7 +146,8 @@ const CategorySpace: React.FC<CategorySpaceProps> = ({
         containerRef.current &&
         !containerRef.current.contains(event.target as Node)
       ) {
-        setShowOptions(false);
+        setShowOuterOptions(false);
+        setShowInnerOptions(false);
       }
     };
 
@@ -109,7 +158,7 @@ const CategorySpace: React.FC<CategorySpaceProps> = ({
   }, [containerRef]);
 
   return (
-    <div className="drawerContent" onContextMenu={handleContextMenu}>
+    <div className="drawerContent" onContextMenu={handleOuterContextMenu}>
       <div className="categorySpace">
         <div className="algorithmList">Task 목록</div>
         {categoryList.map((category) => {
@@ -120,20 +169,44 @@ const CategorySpace: React.FC<CategorySpaceProps> = ({
 
           return (
             <div className="categoryRow" key={category.category_id}>
-              <div style={{ display: "flex", alignItems: "center" }}>
-                <span style={{ marginRight: "0.5rem" }}>{category.title}</span>
-                <span>-----------</span>
-                <img
-                  style={{ marginLeft: "0.5rem" }}
-                  title={`${category.title}}`}
-                  src={
-                    isToggleSelected[category.category_id]
-                      ? CategoryExpansion
-                      : CategoryExpansion2
+              {isEditTitle[category.category_id] ? (
+                <>
+                  <input
+                    type="text"
+                    value={newTitle[category.category_id] || category.title}
+                    onChange={(event) => handleInputChange(event, category)}
+                    maxLength={8}
+                  />
+                  <button onClick={() => handleEditTitle(category.category_id)}>
+                    수정
+                  </button>
+                </>
+              ) : (
+                <div
+                  style={{ display: "flex", alignItems: "center" }}
+                  className="categoryTitle"
+                  onContextMenu={(event) =>
+                    handleInnerContextMenu(event, category.category_id)
                   }
-                  onClick={() => handleToggle(category.category_id)}
-                />
-              </div>
+                >
+                  <span style={{ marginRight: "0.5rem" }}>
+                    {category.title}
+                  </span>
+                  <span>
+                    {getHyphens(category.title ? category.title.length : 0)}
+                  </span>
+                  <img
+                    style={{ marginLeft: "0.5rem" }}
+                    title={`${category.title}}`}
+                    src={
+                      isToggleSelected[category.category_id]
+                        ? CategoryExpansion
+                        : CategoryExpansion2
+                    }
+                    onClick={() => handleToggle(category.category_id)}
+                  />
+                </div>
+              )}
               {isToggleSelected[category.category_id] &&
                 filteredTasks.map((task, index: number) => (
                   <div key={index} className="algorithmProblems">
@@ -148,37 +221,60 @@ const CategorySpace: React.FC<CategorySpaceProps> = ({
             </div>
           );
         })}
-
-        {showOptions &&
-          createPortal(
-            <OptionsContainer
-              ref={containerRef}
-              style={{
-                position: "fixed",
-                top: menuPosition.y,
-                left: menuPosition.x,
-              }}
-            >
-              {user &&
-                selectedStudy &&
-                selectedStudy.host &&
-                user.email === selectedStudy.host.email && (
-                  <button className="optionButton" onClick={handleCreateModal}>
-                    카테고리 생성
-                  </button>
-                )}
-              <button className="optionButton">문제 검색</button>
-            </OptionsContainer>,
-            modalRoot
-          )}
-
-        <CreateCategoryModal
-          isOpen={isCreateModalOpen}
-          onClose={handleCreateModal}
-          onSubmit={handleAddCategory}
-          selectedStudy={selectedStudy}
-        />
       </div>
+      <div style={{ width: "100%", height: "auto" }}></div>
+      {showOuterOptions &&
+        createPortal(
+          <OptionsContainer
+            ref={containerRef}
+            style={{
+              position: "fixed",
+              top: menuPosition.y,
+              left: menuPosition.x,
+            }}
+          >
+            {user &&
+              selectedStudy &&
+              selectedStudy.host &&
+              user.email === selectedStudy.host.email && (
+                <button className="optionButton" onClick={handleCreateModal}>
+                  카테고리 생성
+                </button>
+              )}
+            <button className="optionButton">문제 검색</button>
+          </OptionsContainer>,
+          modalRoot
+        )}
+
+      {showInnerOptions &&
+        user &&
+        selectedStudy &&
+        selectedStudy.host &&
+        user.email === selectedStudy.host.email &&
+        createPortal(
+          <OptionsContainer
+            ref={containerRef}
+            style={{
+              position: "fixed",
+              top: menuPosition.y,
+              left: menuPosition.x,
+              width: "140px",
+            }}
+          >
+            <button className="optionButton" onClick={handleCgTitleEdit}>
+              카테고리 이름 변경
+            </button>
+            <button className="optionButton">카테고리 삭제</button>
+          </OptionsContainer>,
+          modalRoot
+        )}
+
+      <CreateCategoryModal
+        isOpen={isCreateModalOpen}
+        onClose={handleCreateModal}
+        onSubmit={handleAddCategory}
+        selectedStudy={selectedStudy}
+      />
     </div>
   );
 };
