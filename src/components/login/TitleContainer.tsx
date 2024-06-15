2db2d247eb78184d33e174c20ce23627f4d10ef7
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-06-16 01:29:25 +0900

    feat: 회원가입 구현

--- src/components/login/TitleContainer.tsx
+++ src/components/login/TitleContainer.tsx
@@ -1,17 +1,25 @@
 // img
 import Logo from "@/assets/login/study_for_code.png";
 
-import { UserDataContext } from "../../pages/Login/Login";
+// pages
 import { useContext } from "react";
+// types
+import { signupDataType } from "@/types/aboutSignup";
+// apis
+import { ContextData } from "@/pages/Singnup/Signup";
 
 const TitleContainer = () => {
-  const userData = useContext(UserDataContext);
+  const userData = useContext<signupDataType | null>(ContextData);
   console.log("TitleContainer: ", userData);
   return (
-    <div className="logoContainer">
-      <img src={Logo} className="logo" />
-      <span className="projectTitle">StudyForCode</span>
-    </div>
+    <>
+      {userData?.status === 200 ? null : (
+        <div className="logoContainer">
+          <img src={Logo} className="logo" />
+          <span className="projectTitle">StudyForCode</span>
+        </div>
+      )}
+    </>
   );
 };
 
